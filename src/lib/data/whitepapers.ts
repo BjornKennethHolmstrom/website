5a2e87bd540dd0486aa25df2373bc06f867d8763
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2025-12-18 21:47:03 +0100

    Fix typo in Swedish preventing 1933 white paper pdf name

--- src/lib/data/whitepapers.ts
+++ src/lib/data/whitepapers.ts
@@ -35,7 +35,7 @@ export const papersData: Whitepaper[] = [
 		pages: '27',
 		readTime: { en: '35 min' },
 		version: '1.0',
-		pdfPath: { en: `${ggfBase}/resources/whitepapers/Preventing_1933_-_Architecture_of_Democratic_Resilience.pdf`, sv: `${ggfBase}/resources/whitepapers/Att_f%C3%B6rhindra_1933_-_Arkitekturen_f%C3%B6r_demomkratisk_motst%C3%A5ndskraft.pdf` },
+		pdfPath: { en: `${ggfBase}/resources/whitepapers/Preventing_1933_-_Architecture_of_Democratic_Resilience.pdf`, sv: `${ggfBase}/resources/whitepapers/Att_f%C3%B6rhindra_1933_-_Arkitekturen_f%C3%B6r_demokratisk_motst%C3%A5ndskraft.pdf` },
 		coverImage: '/whitepapers/images/preventing-1933-cover.png',
 		readOnlinePath: `${ggfBase}/resources/whitepapers/preventing-1933`,
 		readOnlineExternal: true,
